729af011b3deb31650afd6c36f9ae3099f402623
Raiyan <tajkierhaque@gmail.com>
2023-03-20 23:42:36 +0600

    Logical operators are done [Mongodb]

--- Mongodb/operators.js
+++ Mongodb/operators.js
@@ -5,3 +5,12 @@ Docs : - https://www.mongodb.com/docs/manual/reference/operator/query/
 // Comparison
 /* $eq, $lt, $lte, $gte, $ne, $in */
 db.movies.find({ runtime: { $ne: 60 } });
+
+// Query on embedded documents
+db.movies.find({ "ratings.average": { $gt: 7 } }); // You can go deep nested by using (.)
+db.movies.find({ genres: ["drama"] }); // Array -> data will give exact with this value
+
+// Logical Operators
+db.inventory.find({
+  $and: [{ price: { $ne: 1.99 } }, { price: { $exists: true } }],
+});
