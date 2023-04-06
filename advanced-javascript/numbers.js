1a0ea056993c6eba09d7a39b49beeca959fe1f9f
Raiyan <tajkierhaque@gmail.com>
2023-04-07 01:42:07 +0600

    numbers are explained [numbers/advanced_js]

---
+++ advanced-javascript/numbers.js
@@ -0,0 +1,8 @@
+console.log(Number.parseInt("30px", 10)); // By using these I can convert my number into Hexa, Octal Also I can remove the text only when the string will be the last -> the second argument is the base form of a Number system
+
+console.log(Number.parseFloat("2.4rem", 10)); // it is the same as parseInt
+console.log(Number.isNaN(90)); // check a number
+console.log(Number.isFinite(20)); // better way to check if it us a number
+
+/* Numeric separators */
+const trans = 12_300;
